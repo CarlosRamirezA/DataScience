54a5881acb755740a6dfcc250d07bf8b48e8742b
CarlosRamirezA <carlosramirez2507@hotmail.com>
2020-08-14 17:38:56 -0500

    eliminacion hacia atrs usando p-valores y valores R cuadrado

---
+++ multiple_linear_regression/funciones.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 17:25:39 2020
+
+@author: carlos
+"""
+# Eliminación hacia atrás utilizando solamente p-valores:
+
+
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):    
+    numVars = len(x[0])    
+    for i in range(0, numVars):        
+        regressor_OLS = sm.OLS(y, x).fit()        
+        maxVar = max(regressor_OLS.pvalues).astype(float)        
+        if maxVar > sl:            
+            for j in range(0, numVars - i):                
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
+                    x = np.delete(x, j, 1)    
+    regressor_OLS.summary()    
+    return x 
+
+# Eliminación hacia atrás utilizando  p-valores y el valor de  R Cuadrado Ajustado:
+
+import statsmodels.formula.api as sm
+def backwardEliminationr(x, SL):    
+    numVars = len(x[0])    
+    temp = np.zeros((50,6)).astype(int)    
+    for i in range(0, numVars):        
+        regressor_OLS = sm.OLS(y, x).fit()        
+        maxVar = max(regressor_OLS.pvalues).astype(float)        
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)        
+        if maxVar > SL:            
+            for j in range(0, numVars - i):                
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):                    
+                    temp[:,j] = x[:, j]                    
+                    x = np.delete(x, j, 1)                    
+                    tmp_regressor = sm.OLS(y, x).fit()                    
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)                    
+                    if (adjR_before >= adjR_after):                        
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))                        
+                        x_rollback = np.delete(x_rollback, j, 1)     
+                        print (regressor_OLS.summary())                        
+                        return x_rollback                    
+                    else:                        
+                        continue    
+    regressor_OLS.summary()    
+    return x 
--- multiple_linear_regression/multiple_linear_regression.py
+++ multiple_linear_regression/multiple_linear_regression.py
@@ -11,6 +11,7 @@ Created on Mon Aug  3 21:15:48 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import funcionesML as FML
 
  
 dataset = pd.read_csv('startup.csv')
@@ -59,12 +60,27 @@ y_pred = regression.predict(x_test)
 #Contruir el modelo optimo de RLM utilizando la Eliminacion hacia atras.
 import statsmodels.api as sm
 x = np.append(arr = np.ones((50,1)).astype(int), values = x, axis=1)
-x_opt = np.array (x [:, [0, 1, 2, 3, 4, 5]], dtype = float)
-
 SL = 0.05
+
+x_opt = np.array (x [:, [0, 1, 2, 3, 4, 5]], dtype = float)
 regression_OLS = sm.OLS(endog = y, exog = x_opt).fit()
 regression_OLS.summary()
 
+x_opt = np.array (x [:, [0, 1, 3, 4, 5]], dtype = float)
+regression_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regression_OLS.summary()
+
+x_opt = np.array (x [:, [0, 3, 4, 5]], dtype = float)
+regression_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regression_OLS.summary()
+
+x_opt = np.array (x [:, [ 3, 4, 5]], dtype = float)
+regression_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regression_OLS.summary()
+
+x_opt = np.array (x [:, [3, 4]], dtype = float)
+regression_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+regression_OLS.summary()
 
 
 
