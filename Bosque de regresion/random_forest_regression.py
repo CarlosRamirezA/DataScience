92ac67941224141250d81edf19e8fd7b58d03f83
CarlosRamirezA <carlosramirez2507@hotmail.com>
2020-09-01 22:42:25 -0500

    Random Forest

--- Bosque de regresion/random_forest_regression.py
+++ Bosque de regresion/random_forest_regression.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 26 17:38:24 2020
+Created on Tue Sep  1 22:09:57 2020
 
 @author: carlos
 """
 
-
-# Plantilla de Regresion
+# Regresion Bosques aleatorios 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,35 +21,24 @@ y = dataset.iloc[:, 2].values
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
 """
-"""
-#3 Escalado de variables (escalado: y normalizado)
-from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-sc_y = StandardScaler()
-x = sc_x.fit_transform(x)
-y = sc_y.fit_transform(y.reshape(-1,1))"""
 
-# Ajustar la regresion con el dataset
+
+# Ajustar random forest con el dataset
 
 
 
 
-# Prediccion de nuestros modelos  
+# Prediccion de nuestros modelos con random forest   
 y_pred = regression.predict()
 
 
 
-# Visualizacion de los resultados del Modelo Polinomico
+# Visualizacion de los resultados del Modelo random forest 
 x_grid = np.arange(min(x), max(x), 0.1)
 x_grid = x_grid.reshape(len(x_grid),1)
 plt.scatter(x, y, color = "red")
 plt.plot(x_grid, predict(x_grid, regression.predict(x_grid)), color = "blue")
-plt.title("Modelo de Regresion")
+plt.title("Modelo de Regresion con Random Forest")
 plt.xlabel("Posicion del empleado")
 plt.ylabel("Sueldo (en $)")
 plt.show()
-
-
-
-
-
