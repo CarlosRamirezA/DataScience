bfe7ebe73902f0aa025f7fe686ec7b14e2a0b32d
CarlosRamirezA <carlosramirez2507@hotmail.com>
2020-09-14 18:09:03 -0500

    Naives Bayes

---
+++ Naive Bayes/naive_bayes.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 17:48:28 2020
+
+@author: carlos
+"""
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+ 
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values 
+
+# Dividir el data set en conjuntos de entrenamiento y conjunto de testing
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.25, random_state = 0)
+
+
+# Escalado de variables
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train  = sc_x.fit_transform(x_train)
+x_test = sc_x.fit_transform(x_test)
+
+
+# Ajustar el clasificador con el conjunto de Entrenamiento
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(x_train,y_train)
+
+
+
+
+
+# Prediccion de los resultados con el conjunto de Testing
+y_pred = classifier.predict(x_test)
+
+# Elaborar una matriz de confusion
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
+
+# Representacion grafica de los resultados del algoritmo en el conjunto de entrenamiento
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+            alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x1.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)    
+plt.title('Naive Bayes (Conjunto de Entrenamiento)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Representacion grafica de los resultados del algoritmo en el conjunto de testing
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_test, y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+            alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x1.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)    
+plt.title('Naive Bayes (Conjunto de test)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
